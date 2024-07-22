f69276cd13b01e1d4daa74cf2d1e87ceb95dd4a1
Mateus Macedo <mateusmacedoeng@gmail.com>
2024-07-22 19:27:52 -0300

    init angular

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+
+/**
+ * Aqui ficará TODA lógica do componente header
+ * <header-app></header-app>
+ * ng generate component components/button
+ */
+export class HeaderComponent {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
